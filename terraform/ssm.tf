da7e751df807009352f206cc88cfa8136cdb9382
naokivandit <n.i.vandit0924@gmail.com>
2021-02-24 23:36:57 +0900

    fix terraform

--- terraform/ssm.tf
+++ terraform/ssm.tf
@@ -1,3 +1,27 @@
+#############################################################
+# SSMパラメータストア
+#############################################################
+
+#============================================================
+# Common
+#============================================================
+variable "AWS_ACCESS_KEY_ID" {}
+variable "AWS_SECRET_ACCESS_KEY" {}
+
+resource "aws_ssm_parameter" "access-key-id" {
+  name = "access-key-id"
+  value = var.AWS_ACCESS_KEY_ID
+  type = "SecureString"
+  description = "AWS_ACCESS_KEY_ID"
+}
+resource "aws_ssm_parameter" "secret-access-key" {
+  name = "secret-access-key"
+  value = var.AWS_SECRET_ACCESS_KEY
+  type = "SecureString"
+  description = "AWS_SECRET_ACCESS_KEY"
+}
+
+
 // front
 variable "WORKDIR" {}
 variable "FRONT_PORT" {}
