58a0122db2e424a277f7af8b88cf206f9e1ecc84
naokivandit <n.i.vandit0924@gmail.com>
2021-02-25 23:27:30 +0900

    fix: terraform

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,15 +1,59 @@
-resource "aws_ecr_repository" "nuxt" {
-  name = "nuxt-ecr"
+#########
+#  ECR  #
+#########
 
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-}
-
+#__________  api  __________#
 resource "aws_ecr_repository" "rails" {
   name = "rails-ecr"
+}
+resource "aws_ecr_lifecycle_policy" "rails-policy" {
+  repository = aws_ecr_repository.rails.name
 
-  image_scanning_configuration {
-    scan_on_push = true
+  policy = <<EOF
+  {
+    "rules": [
+      {
+        "rulePriority": 1,
+        "description": "Keep last 30 release tagged images",
+        "selection": {
+          "tagStatus": "tagged",
+          "tagPrefixList": ["latest"],
+          "countType": "imageCountMoreThan",
+          "countNumber": 30
+        },
+        "action": {
+          "type": "expire"
+        }
+      }
+    ]
   }
+EOF
+}
+
+#__________ front __________#
+resource "aws_ecr_repository" "nuxt" {
+  name = "nuxt-ecr"
+}
+resource "aws_ecr_lifecycle_policy" "nuxt-policy" {
+  repository = aws_ecr_repository.nuxt.name
+
+  policy = <<EOF
+  {
+    "rules": [
+      {
+        "rulePriority": 1,
+        "description": "Keep last 30 release tagged images",
+        "selection": {
+          "tagStatus": "tagged",
+          "tagPrefixList": ["latest"],
+          "countType": "imageCountMoreThan",
+          "countNumber": 30
+        },
+        "action": {
+          "type": "expire"
+        }
+      }
+    ]
+  }
+EOF
 }
