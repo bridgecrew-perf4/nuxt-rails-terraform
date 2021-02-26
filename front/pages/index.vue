3d3ce4c9f0e86e1900c5dd857981f83ce9f09fc9
naokivandit <n.i.vandit0924@gmail.com>
2021-02-26 21:55:11 +0900

    fix: vue

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -1,8 +1,18 @@
 <template>
   <div>
     <button type="button" name="button" @click="getMsg">
-      RailsからAPIを取得する
+      RailsからAPIを取得する(通常)
     </button>
+    <button type="button" name="button" @click="getMsg2">
+      RailsからAPIを取得する(ドメイン)
+    </button>
+    <button type="button" name="button" @click="getMs3g">
+      RailsからAPIを取得する(alb)
+    </button>
+    <button type="button" name="button" @click="getMsg4">
+      RailsからAPIを取得する(ローカル)
+    </button>
+
     <div v-for="(msg, i) in msgs" :key="i">
       {{ msg }}
     </div>
@@ -19,7 +29,17 @@ export default {
   methods: {
     getMsg () {
       this.$axios.$get('/api/v1/hello').then(res => this.msgs.push(res))
+    },
+    getMsg2 () {
+      this.$axios.$get('https://nuxt-rails.com:3000/api/v1/hello').then(res => this.msgs.push(res))
+    },
+    getMsg3 () {
+      this.$axios.$get('https://nuxt-rails-alb-1270301701.ap-northeast-1.elb.amazonaws.com:3000/api/v1/hello').then(res => this.msgs.push(res))
+    },
+    getMsg4 () {
+      this.$axios.$get('http://localhost:3000/api/v1/hello').then(res => this.msgs.push(res))
     }
+
   }
 }
 </script>
